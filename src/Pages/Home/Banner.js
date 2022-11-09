a78f8381e097f8b524bdf81e366cf259e4e9c319
Arfanul Hoque Erfan <mderfan3928@gmail.com>
2022-11-09 21:37:17 +0600

    navbar added

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,11 @@
+import React from "react";
+import img1 from "../../assets/images/banner/Banner (2).jpg";
+import img2 from "../../assets/images/banner/Banner-2 (2).jpg";
+import img3 from "../../assets/images/banner/Banner-3.jpg";
+import img4 from "../../assets/images/banner/Banner-4.jpeg";
+
+const Banne = () => {
+  return <div></div>;
+};
+
+export default Banne;
