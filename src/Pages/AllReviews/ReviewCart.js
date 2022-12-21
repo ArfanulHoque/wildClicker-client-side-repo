9015409d44cab52561158515b731917861a33d4b
Arfanul Hoque Erfan <mderfan3928@gmail.com>
2022-12-21 23:36:55 +0600

    review

--- src/Pages/AllReviews/ReviewCart.js
+++ src/Pages/AllReviews/ReviewCart.js
@@ -27,44 +27,6 @@ const ReviewCart = ({ review }) => {
           </tbody>
         </table>
       </div>
-      {/* <div className="overflow-x-auto w-full">
-        <table className="table w-full">
-          <thead>
-            <tr>
-              <th>Image</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Review</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th>
-                <div className="avatar">
-                  <div className="mask mask-squircle w-12 h-12">
-                    <img
-                      src={customerImage}
-                      alt="Avatar Tailwind CSS Component"
-                    />
-                  </div>
-                </div>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div>
-                    <div className="font-bold">{customer}</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                {email}
-                <br />
-              </td>
-              <td>{message}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div> */}
     </div>
   );
 };
